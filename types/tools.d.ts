2a1dec42e0e9c2c76e8e7ec2764fed5a5a737b9d
Benoit Mariat <benoit.mariat@intersec.com>
2022-01-21 15:14:00 +0100

    Build 3.0.9.
    Bug fixes:
        * Fix infinite loop when modifying options
    
    Change-Id: I8933a77042dd0681a1e52a3e9fb1ca973303b9fc

---
+++ types/tools.d.ts
@@ -0,0 +1,21 @@
+/**
+ * Clone the object and its inner properties.
+ * @param obj The object to be clone.
+ * @param refs internal reference to object to avoid cyclic references
+ * @returns a copy of obj
+ */
+export declare function deepClone<T = any>(obj: T, refs?: WeakMap<any, any>): T;
+/**
+ * Escape search string to consider regexp special characters as they
+ * are and not like special characters.
+ * Consider * characters as a wildcards characters (meanings 0 or
+ * more characters) and convert them to .* (the wildcard characters
+ * in Regexp)
+ *
+ * @param  {String} name the original string to convert
+ * @param  {String} [flag] mode to apply for regExp
+ * @return {String} the string ready to use for RegExp format
+ */
+export declare function convertToRegExp(name: string, flag?: string): RegExp;
+/** Does the same as Object.assign but does not replace if value is undefined */
+export declare function assignObject<T>(obj: Partial<T>, ...sourceObjects: Array<Partial<T>>): T;
