62dd28a5cc291480c2fcda57a83b1aa4a34fba91
Benoit Mariat <benoit.mariat@intersec.com>
2023-10-26 12:24:36 +0200

    Build 3.0.22.
    Bug fix:
     * Autodisabled when changing options to only 1 option and then
       selecting this value
     * Upgrade dependencies
    
    Change-Id: I3830d913f67ea088357f9b0e536a02e2304e2b9c

--- dist/selectic.common.js
+++ dist/selectic.common.js
@@ -314,6 +314,11 @@ class SelecticStore {
         }, { deep: true });
         vue.watch(() => this.state.internalValue, () => {
             this.buildSelectedOptions();
+            /* If there is only one item, and the previous selected value was
+             * different, then if we change it to the only available item we
+             * should disable Selectic (user has no more choice).
+             * This is why it is needed to check autoDisabled here. */
+            this.checkAutoDisabled();
         }, { deep: true });
         vue.watch(() => this.state.allOptions, () => {
             this.checkAutoSelect();
